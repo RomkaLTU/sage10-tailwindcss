9b29e7d32c0a990ee7607b2725401540073b4716
RD <romualdas@prodev.lt>
2020-12-06 13:41:46 +0200

    webpack mix browser sync change URL

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,7 +19,7 @@ require('laravel-mix-copy-watched');
 
 mix.setPublicPath('./dist')
     .browserSync({
-        proxy: 'https://wmnt3.test',
+        proxy: 'https://sage.test',
         open: false,
         cors: true,
     });
